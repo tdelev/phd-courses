a91ddef42792bbb88ef702d8784b6bf37d15e931
tdelev <tomce.delev@gmail.com>
2012-06-06 03:24:47 +0200

    Initial commit

---
+++ PhD/Matlab/src/featureNormalize.m
@@ -0,0 +1,18 @@
+function [X_norm, mu, sigma] = featureNormalize(X)
+%	FEATURENORMALIZE Normalizes the features in X 
+%   FEATURENORMALIZE(X) returns a normalized version of X where
+%   the mean value of each feature is 0 and the standard deviation
+%   is 1. This is often a good preprocessing step to do when
+%   working with learning algorithms.
+
+X_norm = X;
+mu = zeros(1, size(X, 2));
+sigma = zeros(1, size(X, 2));    
+
+mu = mean(X);
+X_norm = bsxfun(@minus, X, mu);
+
+sigma = std(X_norm);
+X_norm = bsxfun(@rdivide, X_norm, sigma);
+
+end
